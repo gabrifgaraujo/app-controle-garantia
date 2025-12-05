8b5be4ecd0a40d9e00439c6da26f7a60d19f397c
Bruna353 <brunajob1@gmail.com>
2025-12-05 00:11:34 -0300

    add tela de nota final

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Login from './components/Login.tsx'
 import Cadastro from './components/Cadastro.tsx'
+import CadastroNota from './components/CadastroNota.tsx'
+import './style/CadastroNota.css'
 import EsqueceuSenha from './components/EsqueceuSenha.tsx'
 import './style/Login.css'
 import './style/esqueceu-senha.css'
@@ -14,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cadastro-nota" element={<CadastroNota />} />
         <Route path="/esqueceu-senha" element={<EsqueceuSenha/>}/>
       </Routes>
     </BrowserRouter>
